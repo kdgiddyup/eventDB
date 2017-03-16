cee90177c8836ea59d6ec2e08f3c6aa662db740c
Kelly Davis <kellydavis1974@gmail.com>
2017-03-16 18:48:33 -0400

    created index.html, javascript.js and styles.css files in apprappropriate project directories

---
+++ assets/scripts/javascript.js
@@ -0,0 +1,12 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAmK1XtRt48lGcJC9249vs6gGmNAelrFpQ",
+    authDomain: "eventdb-1f7c6.firebaseapp.com",
+    databaseURL: "https://eventdb-1f7c6.firebaseio.com",
+    storageBucket: "eventdb-1f7c6.appspot.com",
+    messagingSenderId: "330315538394"
+  };
+  firebase.initializeApp(config);
+  var db = firebase.database();
+
+  
