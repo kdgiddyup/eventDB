e980231e836d9eee2b2c47da237f4417f6eb1d43
Kelly Davis <kellydavis1974@gmail.com>
2017-03-16 20:08:25 -0400

    created addMarker function that accepts lat/lng object and places a marker

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -15,20 +15,27 @@ function initMap(){
 	var map = new google.maps.Map(document.getElementById('map'), {
           zoom: 10
       	});
-	// create window object with google maps api
-	var infoWindow = new google.maps.InfoWindow({map: map});
 
  // we want to attempt to center the map at the user's present location
  // Try HTML5 geolocation.
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function(position) {
+        // create geolocation object of user's position
         var userPos = {
           lat: position.coords.latitude,
           lng: position.coords.longitude
         };
-        infoWindow.setPosition(userPos);
-        infoWindow.setContent('You are here \(we think\).');
-        map.setCenter(userPos);
+        // for map center, offset longitude to push map to right
+        var mapCenter = {
+        	lat: position.coords.latitude,
+         	lng: position.coords.longitude-.6
+        };
+
+        // send user position to addMarker function
+		addMarker(userPos, map);
+
+        // update map object with new center location
+        map.setCenter(mapCenter);
       }, function() {
         handleLocationError(true, infoWindow, map.getCenter());
       });
@@ -38,9 +45,24 @@ function initMap(){
     }
   }
 
-  function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+// pass a lat/lng object to this function to place a marker on the map
+function addMarker(pos, thisMap){
+    var marker = new google.maps.Marker({
+      position: pos,
+      map: thisMap
+    });
+	
+	// place infoWindow location at userPos
+  	//infoWindow.setPosition(pos);
+        
+    // message user in the infoWindow
+    //infoWindow.setContent('You are here \(we think\).');
+}
+
+  // this code is straight from google API documentation, for handling errors that occur during attempted browser geolocation
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
     infoWindow.setPosition(pos);
     infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
+      'Error: The Geolocation service failed.' :
+      'Error: Your browser doesn\'t support geolocation.');
   }
