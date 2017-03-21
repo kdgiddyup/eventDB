f9cf2c3527a269b218bce404649f042821c4d815
Kelly Davis <kellydavis1974@gmail.com>
2017-03-21 00:55:01 -0400

    this is a large commit - many map functions added including setting and removing color-styled markers for restaurants and events, functions for adding event info to marker windows and to HTML, accepting date input and setting location for ajax calls based on geolocation.

--- assets/scripts/javascript.js
+++ assets/scripts/javascript.js
@@ -2,28 +2,60 @@
 function initMap() {
     // create map object using google maps api method
     map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 10
-    });
-    var infoWindow = new google.maps.InfoWindow({
-        content: '<p>You are here (we think)</p>'
-    });
-    // we want to attempt to center the map at the user's present location
+        zoom: 11,
+        mapTypeControlOptions: {
+          style: google.maps.MapTypeControlStyle.DEFAULT,
+          mapTypeIds: ['roadmap', 'terrain','hybrid','satellite']
+          },
+        fullscreenControl: true,
+        styles: [
+          {
+            featureType: 'all',
+            stylers: [
+              { saturation: -80 }
+            ]
+          },
+          {
+          featureType: 'road.arterial',
+          elementType: 'geometry',
+          stylers: [
+            { hue: '#00ffee' },
+            { saturation: 50 }
+          ]
+          },
+          {
+          featureType: 'poi.business',
+          elementType: 'labels',                                                                                  
+          stylers: [
+            { visibility: 'off' }                                                                                                                  
+          ]
+          }
+          ] // end styles array
+
+    }); // end initMap
+
+      // add click event to marker to open info window
+    infowindow = new google.maps.InfoWindow({
+        maxWidth: 300
+      });
+
+   // we want to attempt to center the map at the user's present location
     // Try HTML5 geolocation.
     if (navigator.geolocation) {
 
         navigator.geolocation.getCurrentPosition(function(position) {
             // create geolocation object of user's position
-            var userPos = {
+            userPos = {
                 lat: position.coords.latitude,
                 lng: position.coords.longitude
             };
             // on wider screens, offset longitude to push map to right
             if (window.innerWidth > 1000) {
-                var lngOffset = .5
-                var latOffset = -.2;
+                var lngOffset = .2;
+                var latOffset = -.1;
             } else {
-                var lngOffset = 0;
-                var latOffset = .2;
+                var lngOffset = .1;
+                var latOffset = -.1;
             }
             var mapCenter = {
                 lat: position.coords.latitude + latOffset,
@@ -43,6 +75,7 @@ function initMap() {
         // Browser doesn't support Geolocation
         handleLocationError(false, infoWindow, map.getCenter());
     }
+
 }
 
 // function to add multiple markers from search response
@@ -50,9 +83,18 @@ function initMap() {
 // [{type: 'restaurant'|'event', name: '<name>', other response key-value pairs},{result2 object}, {result3 object}] 
 
 function showEvents(resultData) { 
+    // if there are event markers, clear them
+    clearMarkers(eventMarkers);
 
+    // clear events output area and restore header
+    $("#eventOutput").empty().append("<h2>Events</h2>");
+    
+    // reset event markers array
+    eventMarkers = [];
+    
+    // restaurant or event? determines marker color later
+    var type = 'event';
     // resultData should be an array of event objects
-      console.log(resultData.length);
     for (var i=0;i<resultData.length;i++) {
       var thisEvent = resultData[i];
       
@@ -61,34 +103,127 @@ function showEvents(resultData) {
           thisEvent.image = '<p><img class="infoWindowImg" src="'+thisEvent.image+'"/></p>';
 
       // create infoWindow
-      var markerInfo = new google.maps.InfoWindow({
-        content: '<div class="infoWindow"><h2>'+thisEvent.name+'</h2>'+
+      var markerInfo = '<div class="infoWindow"><h2>'+thisEvent.name+'</h2>'+
             thisEvent.image+
             '<p><span class="leadin">Address:</span> '+thisEvent.address+'</p>'+
             '<p><span class="leadin">Start time:</span> '+thisEvent.startTime+'</span></p>'+
             '<p><span class="leadin">Stop time:</span> '+thisEvent.stopTime+'</span></p>'+
             '<p><span class="leadin">More info:</span></p><p class="description">'+thisEvent.description+'</p>'+
             '<p><span class="info_link"><a href="'+thisEvent.url+'" target="_blank">More info and tickets</a></p>'+
-            '</div>'
-          }); // end markerInfo object
+            '</div>'; // end markerInfo
 
 
+// get position and add marker by geocoding the address string; also pass the eventLocation array to receive the marker once created
+    geocode(thisEvent.address,markerInfo,eventMarkers, type);
+   
+   // display events in HTML
+    var eventBlock = $("<div>").addClass('outputBlock');
+    $(eventBlock).append("<h3>"+thisEvent.name+"</h3><p>"+thisEvent.address+"</p><p>Starts: "+thisEvent.startTime+"</p><p>Ends: "+thisEvent.stopTime+"</p><p><a href=\""+thisEvent.url+" target=\"_blank\">More information</a></p>");
+      
+    $("#eventOutput").append(eventBlock);
 
-// get position and add marker by geocoding the address string
-      geocode(thisEvent.address,markerInfo);
     } // end results loop
+
+    // add a marker clusterer library t manage markers that are close together
+    /*
+    var markerCluster = new MarkerClusterer(map, eventMarkers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
+    */
+
+
+} // end show events function
+
+function showRestaurants(resultData) { 
+    // if there are restaurant markers, clear them
+    clearMarkers(restaurantMarkers);
+    
+    // clear restaurant output area and restore header
+    $("#restOutput").empty().append("<h2>Dining</h2>");
+
+    // reset restaurant markers array
+    restaurantMarkers = [];
+    
+    // is this event or restaurant? determines marker color later
+    var type = 'restaurant';
+    // resultData should be an array of restaurant objects
+    for (var i=0;i<resultData.length;i++) {
+      var thisRestaurant = resultData[i];
+
+      // does menu exist? put some html around it, too
+      if (thisRestaurant.menu != '')
+          thisRestaurant.menu = '<p><span class="info_link"><a href="'+thisRestaurant.menu+'" target="_blank">Menu</a></p>';
+
+      // create infoWindow
+      var markerInfo = '<div class="infoWindow"><h2>'+thisRestaurant.name+'</h2>'+
+            '<p><span class="leadin">Address:</span> '+thisRestaurant.location+'</p>'+
+            '<p><span class="leadin">Average cost for two:</span> $'+thisRestaurant.cost+'</span></p>'+
+           thisRestaurant.menu+
+            '</div>'; // end markerInfo
+
+
+
+// get position and add marker by geocoding the address string; also pass the eventLocation array to receive the marker once created
+    geocode(thisRestaurant.location,markerInfo,restaurantMarkers, type);
+
+
+    // display restaurants in HTML
+    var restBlock = $("<div>").addClass('outputBlock');
+    $(restBlock).append("<h3>"+thisRestaurant.name+"</h3><p>"+thisRestaurant.address+"</p><p>Average cost: "+thisRestaurant.cost+"</p>"+thisRestaurant.menu);
+      
+    $("#restOutput").append(restBlock);
+
+    } // end results loop
+
+    // add a marker clusterer library to manage markers that are close together
+    var markerCluster = new MarkerClusterer(map, restaurantMarkers,
+            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 } // end show events function
 
 // pass a lat/lng object ('pos' argument) and infoWindow content ('markerInfo') to this function to place a clickable marker on the map
-function addMarker(pos,windowInfo){
+
+function clearMarkers(markerArray){
+
+  for (i=0;i<markerArray.length;i++){
+    // markerArray[i] is a marker on the map; setting it to no map ('null') makes it vanish
+    markerArray[i].setMap(null);
+  }
+}
+
+function addMarker(pos,windowInfo,_markers,type){
+    // type determines marker color here
+    if (type == 'event') {
+      markerColor='#17B27D';
+      strokeColor='#54FFC4'
+    }
+    else {
+      markerColor = '#B24329';
+      strokeColor = '#FF896D'
+    }
+    // instanstiate a marker object
     var marker = new google.maps.Marker({
         position: pos,
-        map: map
+        map: map,
+        desc: windowInfo,
+        animation: google.maps.Animation.DROP,
+        icon: {                                          
+          path: google.maps.SymbolPath.CIRCLE,
+          scale: 9,
+          fillColor: markerColor,
+          strokeColor: strokeColor,                       
+          strokeWeight: 1,
+          fillOpacity: 1 
+      }//,            
     });
-    // put listener on this marker to open the marker info in an infowindow
-    marker.addListener('click', function() {
-   	windowInfo.open(map, this);
-  	});
+
+      marker.addListener('click', function() {
+         // update infwindow content
+        infowindow.setContent(marker.desc);
+        infowindow.open(map, marker);
+        });
+    // add marker to marker array
+    _markers.push(marker);
+    
+    marker.desc = windowInfo;
   }
 
 // this code is straight from google API documentation, for handling errors that occur during attempted browser geolocation
@@ -101,19 +236,24 @@ function handleLocationError(browserHasGeolocation, infoWindow, pos) {
 
 
 // gets stringy address, returns {lat,lng} object in latLngCallback function
-function geocode(address,info){
+function geocode(address,info,markerArray,type){
   // API call to google geocoding service - takes address (encoded) and api key
   var urlQuery = 'https://maps.googleapis.com/maps/api/geocode/json?address='+encodeURI(address)+'&key=AIzaSyAmK1XtRt48lGcJC9249vs6gGmNAelrFpQ';
   $.ajax({
     url: urlQuery,
     method: 'GET'
   }).done(function(response){
-    addMarker(response.results[0].geometry.location,info);
-  });
+    // using callback to assign marker object to markerCallback
+    addMarker(response.results[0].geometry.location,info,markerArray,type);
+  }); // end ajax done function
 }
 
 // document ready statements here
 $(document).ready(function(){
+
+// it will be necessary to  track markers in order to unset them so here're arrays to help with that
+eventMarkers = [];
+restaurantMarkers = [];
 
 // Initialize Firebase
 var config = {
@@ -125,29 +265,59 @@ var config = {
 };
 firebase.initializeApp(config);
 var db = firebase.database();
+
+
 var events = [];
 
-    $("#eventTester").on("click", function() {
-        // restaurant ajax stuff here
+    $("#submitButton").on("click", function(event) {
+        event.preventDefault();
 
+        // check for required fields
+        var inputs =[];
+        
+        // loop through inputs on the page
+        $("input").each(function(){
+          // remove any previously added 'blank' class; we add the 'blank' class to alert user a field must be filled in; 
+          // if there are no blank classes, jquery will fail silently
+          $(this).removeClass("blank");
+          
+          // add inputs to the inputs array
+          inputs.push($(this))
+        });
+
+        // loop through array and check for required data attribute and blank values
+        for (var i=0;i<inputs.length;i++){
+          if ($(inputs[i]).attr("data-required")=="required") {
+            if ($(inputs[i]).val()=="")
+              $(inputs[i]).addClass("blank")
+          }
+          else {
+
+        // event ajax stuff here
+        // get events close to user position
+        var where = userPos.lat+','+userPos.lng;
+        var when = $("#inputDate").val().replace(/-/g,'')+'00';
+        
+        console.log(when);
         var oArgs = {
 
             app_key: 'GRMfQ3CqpWsGdfXM',
 
             q: "",
 
-            where: "31401",
+            // to do: need an ajax call to get zip from geolocated lat/lng
+
+            where: where,
 
             within: '10',
 
-            "date": "Next Week",
+            date: when+'-'+when,
 
-            page_size: 10,
+            page_size: 15,
 
             sort_order: "popularity",
 
         };
-
         events = [];
 
 
@@ -181,7 +351,7 @@ var events = [];
           }
          address = eventsArr[i].venue_address + ', ' + eventsArr[i].city_name + ', ' + eventsArr[i].region_abbr;
                 // console.log( position )
-                // console.log( address )
+                 console.log( address )
 
           events.push({
                 name: eventsArr[i].title,
@@ -198,38 +368,39 @@ var events = [];
 
     } // end for loop
     showEvents(events);
-    console.log(events);
       }); // end api call
 
- });  // end test click function
+// restaurant ajax call
+    var restData = [];
+    var queryURL = "https://developers.zomato.com/api/v2.1/search?q=&count=15&lat="+userPos.lat+"&lon="+userPos.lng+"&sort=cost&order=asc";
+    var key = "1d78eb50e1194c317037b03a6ab3118e";
+    $.ajax({
+            url: queryURL,
+            method: "GET",
+            beforeSend: function(request) {
+                request.setRequestHeader("user-key", key);
+            },
+        })
+        .done(function(response) {
+            restData = [];
+            results = response.restaurants;
+            //console.log(results);
+            for (var i = 0; i < results.length; i++) {
+                var a = {
+                    name: results[i].restaurant.name,
+                    location: results[i].restaurant.location.address,
+                    cost: results[i].restaurant.average_cost_for_two,
+                    menu: results[i].restaurant.menu_url
+                }; //!a could grow depending on additional info that we need
+                restData.push(a);
+            } //ends for loop
+            showRestaurants(restData);
+            //console.log(restData);
+      }); //ends done function
 
-  var restData = [];
+        } // ends else statements for valid input           
+      } // end input for loop
+ });  // end submit click function
 
-    $("#restTester").on("click",
-        function restSearch() {
-            var queryURL = "https://developers.zomato.com/api/v2.1/search?q=mexican&count=15&lat=32.039347819445&lon=-81.108557106944&sort=cost&order=asc";
-            var key = "1d78eb50e1194c317037b03a6ab3118e";
-            $.ajax({
-                    url: queryURL,
-                    method: "GET",
-                    beforeSend: function(request) {
-                        request.setRequestHeader("user-key", key);
-                    },
-                })
-                .done(function(response) {
-                    restData = [];
-                    results = response.restaurants;
-                    console.log(results);
-                    for (var i = 0; i < results.length; i++) {
-                        var a = {
-                            "name": results[i].restaurant.name,
-                            "location": results[i].restaurant.location.address,
-                            "cost": results[i].restaurant.average_cost_for_two
-                        }; //!a could grow depending on additional info that we need
-                        restData.push(a);
-                    } //ends for loop
-                    console.log(restData);
-                }); //ends done function
-        }); //ends restSearch function
 
 }); // end doc ready
